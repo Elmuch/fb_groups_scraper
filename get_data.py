c346f55179ca3dd1028676b67806dbd3469002a0
elijah <elgachuhi@gmail.com>
2015-01-11 11:17:28 -0500

    Removes unused script

--- get_data.py
+++
@@ -1,9 +0,0 @@
-from facepy import GraphAPI
-
-def export_comments():
-  graph = GraphAPI("CAACEdEose0cBAFIz6tyw6sMamJZAGANaWBndnROuq7bqXblD7uckv5etZCrIMqP7mtZAASZA3264j2ZAok4jhtE5fnpy9WeChJWkwrt28kVUlPPvNxdfsGfNfu5wKBtmHFcuw8YMIh8hIZAt9ByzR2RxEo5mtxdramzgYialzTIFMpBZB8pQ2Ri25IbFIIJUAjKqleNabpuL7c2Unqq91oBAQ2twiM1paQZD")
-
-# with open("text.txt", "w") as textfile:
-#   textfile.write("Success!")
-
-print graph
